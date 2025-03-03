b5905292b566bdc3183aeda709271f1e86975ee7
Najmul Hosain <developer.zeroone@gmail.com>
2025-03-03 12:47:58 +0600

    update

--- secure-sight-frontend/src/helpers/form_helper.js
+++ secure-sight-frontend/src/helpers/form_helper.js
@@ -2,11 +2,11 @@ import { format, getYear, subDays } from "date-fns"
 
 export const getWeeklyReportPayload = (values) => {
   const start_date = subDays(values.selectedDate, 7)
-  const start_day = start_date.getDate()
+  const start_day = format(start_date, 'dd')
   const start_month = format(start_date, 'MMM').toLowerCase()
 
   const end_date = subDays(values.selectedDate, 1)
-  const end_day = end_date.getDate()
+  const end_day = format(end_date, 'dd')
   const end_month = format(end_date, 'MMM').toLowerCase()
 
   const year = getYear(values.selectedDate)
