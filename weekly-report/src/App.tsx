d5b5e296bce9036f225e169dcba5bda71c44d449
Najmul Hosain <developer.zeroone@gmail.com>
2025-03-27 17:24:48 +0600

    update

--- weekly-report/src/App.tsx
+++ weekly-report/src/App.tsx
@@ -25,7 +25,7 @@ function App() {
     ChartDataLabels
   );
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="weekly-reporting">
       <Providers>
         <Router />
       </Providers>
