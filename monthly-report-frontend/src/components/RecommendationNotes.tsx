3e5e6d3a0554d9baed1f0c321474719957ed8db8
Najmul Hosain <developer.zeroone@gmail.com>
2025-04-06 03:28:16 +0600

    update

--- monthly-report-frontend/src/components/RecommendationNotes.tsx
+++ monthly-report-frontend/src/components/RecommendationNotes.tsx
@@ -8,29 +8,29 @@ interface RecommendationNotesProps {
 export default function RecommendationNotes({
   notes,
 }: RecommendationNotesProps) {
-  const NoteList = useMemo(() => {
-    return notes.map((note, i) => (
-      <div key={i}>
-        <p
-          style={{
-            fontSize: 26,
-            fontWeight: "bold",
-            color: "#090c9b",
-            marginBottom: 0,
-          }}
-        >
-          {note.key}
-        </p>
-        <ul>
-          {note.data.map((j, k) => (
-            <li style={{ fontSize: 24 }} key={k}>
-              {j}
-            </li>
-          ))}
-        </ul>
-      </div>
-    ));
-  }, [notes]);
-
-  return <div>{NoteList}</div>;
+  return (
+    <div>
+      {notes.map((note, i) => (
+        <div key={i}>
+          <p
+            style={{
+              fontSize: 26,
+              fontWeight: "bold",
+              color: "#090c9b",
+              marginBottom: 0,
+            }}
+          >
+            {note.key}
+          </p>
+          <ul>
+            {note.data.map((j, k) => (
+              <li style={{ fontSize: 24 }} key={k}>
+                {j}
+              </li>
+            ))}
+          </ul>
+        </div>
+      ))}
+    </div>
+  );
 }
