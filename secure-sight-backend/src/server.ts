5de675456b0da19df0401062d4630d8b092fbbc8
Najmul Hosain <developer.zeroone@gmail.com>
2025-07-15 07:31:40 +0600

    start schedule job in seperate process

--- secure-sight-backend/src/server.ts
+++ secure-sight-backend/src/server.ts
@@ -1,11 +1,8 @@
 import express, { ErrorRequestHandler, Request, Response } from "express";
 import build from "./app";
 import path from "path";
-import scheduler from './helper/cron.helper'
 
 build().then((app) => {
-  scheduler.start()
-
   // Serve static files in production
   if (process.env.NODE_ENV === 'prod') {
     app.use(express.static(path.resolve(__dirname, "../../client/build")))
@@ -16,25 +13,5 @@ build().then((app) => {
 
   const PORT = process.env.PORT || 5001
   app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
-
-  /**
-   * Gracefully stop job scheduler
-   */
-  async function graceful() {
-    await scheduler.stop();
-    process.exit(0);
-  }
-
-  process.on('SIGTERM', graceful);
-  process.on('SIGINT', graceful);
-  process.on('unhandledRejection', (reason, promise) => {
-    console.error('Unhandled Rejection at:', promise, 'reason:', reason);
-    graceful();
-  });
-
-  process.on('uncaughtException', (error) => {
-    console.error('Uncaught Exception thrown', error);
-    graceful();
-  });
 
 })
---
+++ secure-sight-backend/src/start-scheduler.ts
@@ -0,0 +1,27 @@
+import build from "./app";
+import scheduler from "./helper/cron.helper";
+
+build().then((app) => {
+  scheduler.start()
+  
+  
+  /**
+   * Gracefully stop job scheduler
+   */
+  async function graceful() {
+    await scheduler.stop();
+    process.exit(0);
+  }
+
+  process.on('SIGTERM', graceful);
+  process.on('SIGINT', graceful);
+  process.on('unhandledRejection', (reason, promise) => {
+    console.error('Unhandled Rejection at:', promise, 'reason:', reason);
+    graceful();
+  });
+
+  process.on('uncaughtException', (error) => {
+    console.error('Uncaught Exception thrown', error);
+    graceful();
+  });
+})
