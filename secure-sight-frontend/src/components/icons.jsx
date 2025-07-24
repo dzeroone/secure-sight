3cc35d8fc9e49a83f9affe8e5b32541014a93e44
Najmul Hosain <developer.zeroone@gmail.com>
2025-07-25 02:10:33 +0600

    update

--- secure-sight-frontend/src/components/icons.jsx
+++ secure-sight-frontend/src/components/icons.jsx
@@ -1,40 +1,11 @@
-import { createLucideIcon } from "lucide-react";
-export const WindowsIcon = createLucideIcon("windows", [
-  [
-    "path",
-    {
-      "d": "m43.489 43.689-20.012-.598V16.894l20.012 3.359z",
-      "key": "1v43eb"
-    }
-  ],
-  [
-    "path",
-    {
-      "d": "m43.489 73.009-20.012 4.526V51.04l20.012-.199z",
-      "key": "302zgi"
-    }
-  ],
-  [
-    "path",
-    {
-      "d": "m70.951 44.486-20.012-.697V21.504l20.012 3.359z",
-      "key": "1ypb7u"
-    }
-  ],
-  [
-    "path",
-    {
-      "d": "M70.951 66.795 50.939 71.32V50.841l20.012-.199z",
-      "key": "1crttk"
-    }
-  ],
-  [
-    "rect",
-    {
-      "width": "94.43",
-      "height": "94.43",
-      "rx": "2.89",
-      "key": "wt3wbh"
-    }
-  ]
-]);
+import { Icon } from "lucide-react";
+export const WindowsIcon = () => {
+  return <Icon iconNode={
+    [
+      ["path",
+      {
+        d: "M0 36.357L104.62 22.11l.045 100.914-104.57.595L0 36.358zm104.57 98.293l.08 101.002L.081 221.275l-.006-87.302 104.494.677zm12.682-114.405L255.968 0v121.74l-138.716 1.1V20.246zM256 135.6l-.033 121.191-138.716-19.578-.194-101.84L256 135.6z"
+      }]
+    ]
+  } viewBox="-0.5 0 257 257" stroke="none" fill="currentColor" />
+};
